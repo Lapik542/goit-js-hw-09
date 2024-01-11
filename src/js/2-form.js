cd6b89ddec59cd20654d694034d0fa903b0283c3
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-01-11 22:30:06 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,31 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
   const feedbackForm = document.querySelector('.feedback-form');
+  const emailInput = feedbackForm.querySelector('input[name="email"]');
+  const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
 
-  const storedFormData =
-    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-  if (storedFormData.email) {
-    feedbackForm.querySelector('input[name="email"]').value =
-      storedFormData.email;
-  }
-  if (storedFormData.message) {
-    feedbackForm.querySelector('textarea[name="message"]').value =
-      storedFormData.message;
-  }
-
-  feedbackForm.addEventListener('input', function (event) {
+  const trimAndSaveToLocalStorage = () => {
     const formData = {
-      email: feedbackForm.querySelector('input[name="email"]').value,
-      message: feedbackForm.querySelector('textarea[name="message"]').value,
+      email: emailInput.value.trim(),
+      message: messageTextarea.value.trim(),
     };
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  };
+
+  const populateFormFromLocalStorage = () => {
+    const storedFormData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+    if (storedFormData.email) {
+      emailInput.value = storedFormData.email;
+    }
+    if (storedFormData.message) {
+      messageTextarea.value = storedFormData.message;
+    }
+  };
+
+  feedbackForm.addEventListener('input', function () {
+    trimAndSaveToLocalStorage();
   });
 
   feedbackForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const formData = {
-      email: feedbackForm.querySelector('input[name="email"]').value,
-      message: feedbackForm.querySelector('textarea[name="message"]').value,
+      email: emailInput.value.trim(),
+      message: messageTextarea.value.trim(),
     };
 
     console.log('Form Data:', formData);
@@ -33,4 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
     localStorage.removeItem('feedback-form-state');
     feedbackForm.reset();
   });
+   
+  populateFormFromLocalStorage();
 });
